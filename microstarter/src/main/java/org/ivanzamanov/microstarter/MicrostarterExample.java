32f5b15d4c1d8d6098d53b9cc6cabac99f40e922
Ivan Zamanov <izamanov@attraqt.com>
2018-03-03 10:34:21 +0200

    Add docker image build

--- microstarter/src/main/java/org/ivanzamanov/microstarter/MicrostarterExample.java
+++ microstarter/src/main/java/org/ivanzamanov/microstarter/MicrostarterExample.java
@@ -1,12 +1,16 @@
 package org.ivanzamanov.microstarter;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
+@EnableAutoConfiguration
 public class MicrostarterExample {
 
-    @RequestMapping("/")
+    @RequestMapping("/hello")
     @ResponseBody
     public String hello() {
         return "hello";
